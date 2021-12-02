757da026f91da0ca1ad81ac9c086e56b1f1904f2
mengjiewarmuth <mengjie.warmuth@achter-beratung.de>
2021-12-02 17:31:45 +0100

    coinsea-templet

---
+++ configuration.ts
@@ -0,0 +1,9 @@
+export const configuration = {
+  header: {
+    heading: 'Coinsea',
+    headingtext:
+      'Hallo, we come from another World and we want to make freind with you. Let us make the world better',
+    buttontext: 'Details',
+    buttonlink: '\home'
+  }
+};
